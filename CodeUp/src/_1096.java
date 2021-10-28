645f2322076a64e6d6d45d72d585d69a090a4c18
zeroh0 <umberto1900@naver.com>
2021-10-29 00:52:24 +0900

    CodeUp

--- CodeUp/src/_1096.java
+++ CodeUp/src/_1096.java
@@ -16,7 +16,9 @@ public class _1096 {
         int[][] board = new int[19][19];
 
         for(int i=0; i<in.length; i++) {
-            board[in[i][0]-1][in[i][1]-1]++;
+            if (board[in[i][0]-1][in[i][1]-1] == 0) {
+                board[in[i][0]-1][in[i][1]-1]++;
+            }
         }
 
         for(int i=0; i<board.length; i++) {
