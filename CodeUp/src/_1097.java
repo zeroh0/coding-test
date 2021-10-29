3bb5a924a89902b65f372f873988b01e9f83f067
zeroh0 <umberto1900@naver.com>
2021-10-30 04:55:11 +0900

    CodeUp

--- CodeUp/src/_1097.java
+++ CodeUp/src/_1097.java
@@ -5,43 +5,27 @@ public class _1097 {
         //input
         Scanner scanner = new Scanner(System.in);
         int[][] board = new int[19][19];
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[i].length; j++) {
+        for(int i=0; i<board.length; i++)
+            for(int j=0; j<board[i].length; j++)
                 board[i][j] = scanner.nextInt();
-            }
-        }
+
         int n = scanner.nextInt();
-        int[][] in = new int[n][n];
-        for(int i=0; i<in.length; i++) {
-            for(int j=0; j<in[i].length; j++) {
+
+        int[][] in = new int[n][2];
+        for(int i=0; i<in.length; i++)
+            for(int j=0; j<in[i].length; j++)
                 in[i][j] = scanner.nextInt();
+
+        //outputs
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[i].length; j++) {
+
             }
         }
 
-        /*
-        in.length = 2, in[i].length = 2
-        in[0][0] = 10, in[0][1] = 10
-        in[1][1] = 12, in[1][1] = 12
-        */
-
-        //output
-        for(int i=0; i<board.length; i++) {
-            if(i < in.length) {
-                if (board[i][in[i][1]] > 0) {
-                    board[i][in[i][1]]--;
-                } else {
-                    board[i][in[i][1]]++;
-                }
-            }
-            for(int j=0; j<board[i].length; j++) {
-                if(i < in.length) {
-                    if(board[in[i][0]-1][in[i][1]-1] > 0) {
-                        board[in[i][0]-1][in[i][1]-1]--;
-                    } else {
-                        board[in[i][0]-1][in[i][1]-1]++;
-                    }
-                }
-                System.out.print(board[i][j] + " ");
+        for(int a[]:board) {
+            for(int b:a) {
+                System.out.print(b + "  ");
             }
             System.out.println();
         }
