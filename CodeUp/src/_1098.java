c63f55bcc53f4b4202142bf58cb3023c4452a291
zeroh0 <umberto1900@naver.com>
2021-10-30 22:41:13 +0900

    CodeUp

--- CodeUp/src/_1098.java
+++ CodeUp/src/_1098.java
@@ -12,17 +12,25 @@ public class _1098 {
         // 2. 입력한 수만큼 값 입력가능
         int in = sc.nextInt();
         int[][] input = new int[in][4];
-        for(int i=0; i<input.length; i++)
-            for(int j=0; j<input.length; j++)
+        for(int i=0; i<input.length; i++) {
+            for (int j=0; j<input[i].length; j++) {
                 input[i][j] = sc.nextInt();
+            }
+        }
 
         // 3. 입력한 값의 크기에 맞는 board 생성
         int[][] board = new int[size[0]][size[1]];
 
-        System.out.println();
+        // 출력
+        int x = 0, y= 0;
         for(int i=0; i<board.length; i++) {
+            // 막대의 좌표
+            if(i < in) {
+                x = input[i][2]-1;
+                y = input[i][3]-1;
+            }
             for(int j=0; j<board[i].length; j++) {
-                System.out.print(board[i][j] + "  ");
+//                System.out.print(board[i][j] + "  ");
             }
             System.out.println();
         }
