00165f2d110b808bad49a89054f0e08df2711084
zeroh0 <umberto1900@naver.com>
2021-11-13 00:07:25 +0900

    greedy

--- Algorithm/src/greedy/UntilItBecomes1.java
+++ Algorithm/src/greedy/UntilItBecomes1.java
@@ -10,19 +10,21 @@ public class UntilItBecomes1 {
         int k = sc.nextInt();
 
         int cnt = 0;
-        if (n % k == 0) {
-            while (n != 1) {
+
+        while (n != 1) {
+            if (n % k == 0) {
                 n /= k;
                 cnt++;
-            }
-        } else {
-            n -= 1;
-            cnt++;
-            while (n != 1) {
-                n /= k;
+            } else {
+                n -= 1;
                 cnt++;
+                if (n % k == 0) {
+                    n /= k;
+                    cnt++;
+                }
             }
         }
         System.out.println(cnt);
+
     }
 }
