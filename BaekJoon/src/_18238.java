dd1e7c3180ffa821e34ef2099e1d5270711a5e6f
zeroh0 <umberto1900@naver.com>
2021-12-12 02:29:23 +0900

    BackJoon

--- BaekJoon/src/_18238.java
+++ BaekJoon/src/_18238.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /*
@@ -35,20 +37,25 @@ import java.util.Scanner;
 public class _18238 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next(); // 문자열 입력
-        char[] c = {
+        char[] c = sc.next().toCharArray(); // 문자열 입력
+        char[] alpha = {
                 'A', 'B', 'C', 'D', 'E', 'F',
                 'G', 'H', 'I', 'J', 'K', 'L',
                 'M', 'N', 'O', 'P', 'Q', 'R',
                 'S', 'T', 'U', 'V', 'W', 'X',
                 'Y', 'Z'
         };
-        // 0 1 2 3 4 5 6 7 8 9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-        // A B C D E F G H I J  K  L  M  N  O  P  Q  L  S  T  U  V  W  X  Y  Z = 26
-        final int HALF = 11;
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+        // A B C D E F G H I J K  L  M  N  O  P  Q  L  S  T  U  V  W  X  Y  Z = 26
+        final int HALF = 12;
         final int LAST = 25;
         int start = 0;
 
+        int index = 0;
+        for(char a:c) {
+            index = Arrays.binarySearch(alpha, a);
+            System.out.println(index);
+        }
 
     } // main()
 }
